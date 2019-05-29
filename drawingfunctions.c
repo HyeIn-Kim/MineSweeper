5f43ffe05130b12283d7adac9ffd0f2c23bcb928
hann3 <soyo_0131@naver.com>
2019-05-30 02:35:34 +0900

    Add comment

--- drawingfunctions.c
+++ drawingfunctions.c
@@ -1,12 +1,22 @@
 ﻿#include "Minesweeper.h"
 
+
+/** 
+* 게임판을 그리는 함수
+* 게임이 진행되는 동안 play 함수 내에서 꾸준히 호출된다
+* 블록의 상태를 시각적으로 나타내는 일을 한다
+* @param void
+* @return void 
+*/
+
 void DrawBoard(void) {
 	int i, j;
-	COORD pos;
+	COORD pos;	/* 현재 커서의 위치 */
+	/* 호출시 설정한 위치로 커서를 옮긴다. 게임판은 고정된 위치에서 그려진다 */
 	SetCurrentCursorPos(OFFSET_X, OFFSET_Y);
 
 	for (i = 0; i < boardRows; i++) {
-
+		/* 범위 밖 블록은 그리지 않음 */
 		if (my_Board[i][0].statusBlock == STATUS_OUTOFRANGE) {
 			return;
 		}
@@ -17,9 +27,11 @@ void DrawBoard(void) {
 			switch (my_Board[i][j].statusBlock) {
 			case STATUS_OPEN:
 				if (my_Board[i][j].statusMine == MINE) {
+					/* 지뢰 */
 					printf("★");
 				}
 				else {
+					/* 지뢰가 아닌 경우 인접 지뢰 수를 표기 */
 					printf("%d ", my_Board[i][j].cntNearMine);
 				}
 				break;
@@ -35,13 +47,14 @@ void DrawBoard(void) {
 			default:
 				break;
 			}
+			/* 커서를 오른쪽으로 두 칸 옮긴다. x축의 이동 단위는 2 */
 
 			SetCurrentCursorPos(pos.X + 4, pos.Y);
 		}
 
 		printf("\n");
 		pos = GetCurrentCursorPos();
-
+		/* 커서를 아래쪽으로 한 칸 옮긴다. y축의 이동 단위는 1 */
 		SetCurrentCursorPos(pos.X + OFFSET_X, pos.Y + 1);
 	}
 }
--- utilityfunctions.c
+++ utilityfunctions.c
@@ -1,35 +1,68 @@
 ﻿#include "Minesweeper.h"
 #pragma warning (disable:4996)
 
+/** 
+* 입력 받은 행의 사이즈가 범위 내의 수인지 여부를 체크하는 함수 
+* @param void
+* @return 행이 범위 내 수일 경우 1, 아닐 경우 0
+*/
+
 int CheckRows(void) {
 	return (boardRows >= MINBOARD && boardRows <= MAXBOARD);
 }
+
+/** 
+* 입력 받은 열의 사이즈가 범위 내의 수인지 여부를 체크하는 함수 
+* @param void
+* @return 열이 범위 내 수일 경우 1, 아닐 경우 0
+*/
 
 int CheckCols(void) {
 	return (boardCols >= MINBOARD && boardCols <= MAXBOARD);
 }
 
+/**
+* 행이 게임판 내부의 행인지 여부를 체크하는 함수
+* @param int currentRow 현재 행 번호
+* @return 게임판 내부의 행일 경우 1, 아닐 경우 0
+*/
+
 int CheckRowsinGame(int currentRow) {
 	return (currentRow >= 0 && currentRow < boardRows);
 }
+
+/**
+* 열이 게임판 내부의 열인지 여부를 체크하는 함수
+* @param int currentCol 현재 열 번호
+* @return 게임판 내부의 열일 경우 1, 아닐 경우 0
+*/
 
 int CheckColsinGame(int currentCol) {
 	return (currentCol >= 0 && currentCol < boardCols);
 }
 
+
+/**
+* 재시작 여부를 묻는 함수
+* @param void
+* @return 최초 실행시 1, 이후에는 사용자에게 여부를 묻고 반환
+*/
+
 int IsGameReset(void){
-	static int flag = 0;
+	static int flag = 0;	/* 최초 실행여부 */
 	char user;
 	COORD pos;
 	
 	user = ' ';
-
+	
+	/* 최초 실행시 여부를 묻지 않고 true 반환 */
 	if(flag == 0) {
 		flag = 1; 
 		return 1;
 	}
 	pos = GetCurrentCursorPos();
 
+	/* 사용자가 올바른 값을 입력할 때까지 무한 반복 */
 	while(user != 'y' && user != 'Y' && user != 'n' && user != 'N') {
 
 		SetCurrentCursorPos(pos.X, pos.Y);
@@ -40,6 +73,7 @@ int IsGameReset(void){
 		scanf("%c", &user);
 
 		if(user == 'y' || user == 'Y') {
+			/* 재시작시 화면을 비운다 */
 			system("cls");
 			return 1;
 		}
@@ -49,9 +83,15 @@ int IsGameReset(void){
 		}
 	}
 
-	/* error-loop ended before getting right input */
+	/* 비정상적인 예외 상황 */
 	return -1;
 }
+
+/**
+* 사용자의 행동이 1(open) 또는 2(flag) 올바른 입력인지 체크하는 함수
+* @param int action 사용자의 입력값
+* @return 사용자의 입력이 올바른 입력일 경우 1, 아닐 경우 0
+*/
 
 int ActionInRange(int action) {
 	if (action == 1 || action == 2) {
@@ -64,16 +104,28 @@ int ActionInRange(int action) {
 		
 }
 
+/**
+* 원하는 좌표를 입력 받아 커서를 해당 위치로 이동시키는 함수
+* @param int x, int y 각각의 x, y 좌표
+* @return void
+*/
+
 void SetCurrentCursorPos(int x, int y) {
 	COORD pos = { x, y };
 
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
 
+/**
+* 현재 커서가 위치한 좌표를 반환하는 함수
+* @param void
+* @return 현재 커서의 위치를 담고 있는 2차원 좌표계
+*/
+
 COORD GetCurrentCursorPos(void) {
 	COORD curPoint;
-	CONSOLE_SCREEN_BUFFER_INFO curInfo;
-
+	CONSOLE_SCREEN_BUFFER_INFO curInfo;	/* 콘솔 정보 */
+	/* 화면에 띄우는 콘솔 정보를 받아와 변수에 저장 */
 	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &curInfo);
 	curPoint.X = curInfo.dwCursorPosition.X;
 	curPoint.Y = curInfo.dwCursorPosition.Y;
